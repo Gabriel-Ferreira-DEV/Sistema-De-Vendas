fd6720df5e6c571b7207ee8b835b079a42992bff
Gabriel Souza <gabrielsouza6835@gmail.com>
2022-11-16 11:38:51 -0300

    Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
